1dcd4c0d1fc3702f65e489199d6d535d2e677128
PegasusPrime <riverrat87@gmail.com>
2018-07-20 15:59:56 -0400

    update reader

--- reader.pl
+++ reader.pl
@@ -5,9 +5,9 @@ use JSON;
 use LWP::Simple;
 my $ua = LWP::UserAgent->new;
 my $url = 'url.domain.here';
-my ($login_url, $realm, $user, $pass) = ("$url:443", "Blacklist", "etcduser", "bl0cked4u");
-$ua->credentials($login_url, $realm, $user, $pass);
-my $resp = $ua->get( 'https://' . $url );
+my $req = HTTP::Request->new( GET => "https://$url:443/url");
+$req->authorization_basic( "etcd", "bl0cked4u" );
+my $resp = $ua->request($req);
 if (! $resp->is_success) {
  die "Invalid response\n$resp->decoded_content";
 }
